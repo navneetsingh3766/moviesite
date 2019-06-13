566c822a0560682aa96707e0aa13f38ddbe15161
navneet <navneet.singh.3766@gmail.com>
2019-06-13 14:57:54 +0530

    add both static2

--- moviewebsite/settings.py
+++ moviewebsite/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/moviehome/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
