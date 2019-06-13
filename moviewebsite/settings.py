d8600f222717e55e3d534c875868360349f53a74
navneet <navneet.singh.3766@gmail.com>
2019-06-13 14:56:31 +0530

    add both static2

--- moviewebsite/settings.py
+++ moviewebsite/settings.py
@@ -134,5 +134,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
