aee587e48d43281673d9178f28ebc86c9e6ce02f
navneet <navneet.singh.3766@gmail.com>
2019-06-13 14:49:32 +0530

    add both static2

--- moviewebsite/settings.py
+++ moviewebsite/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-'/moviehome/static/',
+    os.path.join(BASE_DIR, 'static'),
+    '/moviehome/static/',
 )
 
 
